bfb258420a098110e6ffe2e6221c4dd8ba86dfd3
Nathaelle <scriptales.clients@gmail.com>
2020-11-26 16:09:23 +0100

    Coreection tableaux articles

---
+++ articles.php
@@ -0,0 +1,30 @@
+<?php
+
+$article1 = [
+    "titre" => "Lorem ipsum dolor sit amet.",
+    "description" => "Culpa, distinctio inventore quia, quasi doloremque perferendis quaerat quisquam sunt consequatur alias nobis praesentium, unde nulla aut adipisci doloribus. ",
+    "image" => "art1.webp",
+    "auteur" => "Jean Dupond",
+    "date" => "26/11/2020"
+];
+
+$article2 = [
+    "titre" => "Sequi perspiciatis labore.",
+    "description" => "Culpa, distinctio inventore quia, quasi doloremque perferendis quaerat quisquam sunt consequatur alias nobis praesentium, unde nulla aut adipisci doloribus. ",
+    "image" => "art2.jpg",
+    "auteur" => "Jeanne Dumas",
+    "date" => "24/11/2020"
+];
+
+$article3 = [
+    "titre" => "Id dolore minima laboriosam eligendi aliquam sit.",
+    "description" => "Culpa, distinctio inventore quia, quasi doloremque perferendis quaerat quisquam sunt consequatur alias nobis praesentium, unde nulla aut adipisci doloribus. ",
+    "image" => "art3.webp",
+    "auteur" => "Tom Smith",
+    "date" => "25/11/2020"
+];
+
+$articles = [$article1, $article2, $article3];
+
+var_dump($articles);
+
--- html/page1.php
+++ html/page1.php
@@ -1,3 +1,9 @@
+<?php
+
+require_once "articles.php";
+
+?>
+
 <h2>Les news</h2>
 
 <article>
@@ -5,33 +11,5 @@
     <p class="news main">Culpa, distinctio inventore quia, quasi doloremque perferendis quaerat quisquam sunt consequatur alias nobis praesentium, unde nulla aut adipisci doloribus. <a href="index.php?page=article&art=0">Lire la suite...</a></p>
     <div class="art">
         <div class="img-cont"><img class="img-resp" src="img/art1.webp" alt=""></div>
-    </div>
-</article>
-<article>
-    <h3>Sequi perspiciatis labore.</h3>
-    <p class="news main">Culpa, distinctio inventore quia, quasi doloremque perferendis quaerat quisquam sunt consequatur alias nobis praesentium, unde nulla aut adipisci doloribus. <a href="index.php?page=article&art=1">Lire la suite...</a></p>
-    <div class="art">
-        <div class="img-cont"><img class="img-resp" src="img/art2.jpg" alt=""></div>
-    </div>
-</article>
-<article>
-    <h3>Id dolore minima laboriosam eligendi aliquam sit.</h3>
-    <p class="news main">Culpa, distinctio inventore quia, quasi doloremque perferendis quaerat quisquam sunt consequatur alias nobis praesentium, unde nulla aut adipisci doloribus. <a href="index.php?page=article&art=2">Lire la suite...</a></p>
-    <div class="art">
-        <div class="img-cont"><img class="img-resp" src="img/art3.webp" alt=""></div>
-    </div>
-</article>
-<article>
-    <h3>Tenetur, asperiores? Vero maxime ab iusto.</h3>
-    <p class="news main">Culpa, distinctio inventore quia, quasi dolorem que perferendis quaerat quisquam sunt consequatur alias nobis praesentium, unde nulla aut adipisci doloribus. <a href="index.php?page=article&art=3">Lire la suite...</a></p>
-    <div class="art">
-        <div class="img-cont"><img class="img-resp" src="img/art4.webp" alt=""></div>
-    </div>
-</article>
-<article>
-    <h3>Numquam veniam officiis.</h3>
-    <p class="news main">Culpa, distinctio inventore quia, quasi doloremque perferendis quaerat quisquam sunt consequatur alias nobis praesentium, unde nulla aut adipisci doloribus. <a href="index.php?page=article&art=4">Lire la suite...</a></p>
-    <div class="art">
-        <div class="img-cont"><img class="img-resp" src="img/art5.webp" alt=""></div>
     </div>
 </article>
